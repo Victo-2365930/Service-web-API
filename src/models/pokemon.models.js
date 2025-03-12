0caabc6f54b4ab6ea6e59bfe2d1f1f5b326ee71a
Yohan <y.rajotte@hotmail.com>
2025-03-12 14:24:31 -0400

    De Mysql @ PostGre

--- src/models/pokemon.models.js
+++ src/models/pokemon.models.js
@@ -1,19 +1,19 @@
-import db from '../config/db.js';
+import sql from "../config/pg_db.js";
 
 
 const pokemondInfoParId = (id) =>{
     return new Promise((resolve, reject) => {
-        const requete = 'SELECT nom, type_primaire, type_secondaire, pv, attaque, defense FROM pokemon WHERE id = ?';
+        const requete = 'SELECT nom, type_primaire, type_secondaire, pv, attaque, defense FROM pokemon WHERE id = $1';
         const params = [id];
 
         db.query(requete, params, (erreur, resultat) => {
             if(erreur){
-                console.log(erreur)
+                console.log('Erreur sqlState : ' + erreur);
+                console.log(`Erreur sqlState ${erreur.sqlState} : ${erreur.sqlMessage}`);
                 reject(erreur);
-
             }
             else{
-                resolve(resultat);
+                resolve(resultat.rows);
             }
         })
     })
@@ -25,10 +25,12 @@ const listerTousPokemons = () =>{
 
         db.query(requete, (erreur, resultat) => {
             if(erreur){
+                console.log('Erreur sqlState : ' + erreur);
+                console.log(`Erreur sqlState ${erreur.sqlState} : ${erreur.sqlMessage}`);
                 reject(erreur);
             }
             else{
-                resolve(resultat);
+                resolve(resultat.rows);
             }
         })
     })
@@ -36,16 +38,17 @@ const listerTousPokemons = () =>{
 
 const listerPokemonType = (type) =>{
     return new Promise((resolve, reject) => {
-        const requete = 'SELECT nom, type_primaire, type_secondaire, pv, attaque, defense FROM pokemon WHERE type_primaire = ?'; //faire par type primaire seulement
+        const requete = 'SELECT nom, type_primaire, type_secondaire, pv, attaque, defense FROM pokemon WHERE type_primaire = $1'; //faire par type primaire seulement
         const params = [type];
 
         db.query(requete, params, (erreur, resultat) => {
             if(erreur){
-                console.log(erreur)
+                console.log('Erreur sqlState : ' + erreur);
+                console.log(`Erreur sqlState ${erreur.sqlState} : ${erreur.sqlMessage}`);
                 reject(erreur);
             }
             else{
-                resolve(resultat);
+                resolve(resultat.rows);
             }
         })
     })
@@ -53,14 +56,16 @@ const listerPokemonType = (type) =>{
 
 const ajouterLePokemon = (req) => {
     return new Promise((resolve, reject) => {
-        const requete = 'INSERT INTO pokemon(nom, type_primaire, type_secondaire, pv, attaque, defense) VALUES(?,?,?,?,?,?)';
+        const requete = 'INSERT INTO pokemon(nom, type_primaire, type_secondaire, pv, attaque, defense) VALUES($1,$2,$3,$4,$5,$6)';
         const params = [req.body.nom, req.body.type_primaire, req.body.type_secondaire, req.body.pv, req.body.attaque, req.body.defense];
 
         db.query(requete, params, (erreur, resultat) => {
             if (erreur) {
+                console.log('Erreur sqlState : ' + erreur);
+                console.log(`Erreur sqlState ${erreur.sqlState} : ${erreur.sqlMessage}`);
                 reject(erreur);
             } else {
-                resolve(resultat);
+                resolve(resultat.rows);
             }
         });
     });
@@ -68,15 +73,17 @@ const ajouterLePokemon = (req) => {
 
 const modifierLePokemon = (req) =>{
     return new Promise((resolve, reject) => {
-        const requete = 'UPDATE pokemon SET(nom = ?, type_primaire = ?, type_secondaire= ?, pv= ?, attaque = ?, defense= ?) WHERE id=?';
+        const requete = 'UPDATE pokemon SET(nom = $1, type_primaire = $2, type_secondaire= $3, pv= $4, attaque = $5, defense= $6) WHERE id=$7';
         const params = [req.body.nom, req.body.type_primaire, req.body.type_secondaire, req.body.pv, req.body.attaque, req.body.defense, req.params.id];
 
         db.query(requete, params, (erreur, resultat) => {
             if(erreur){
+                console.log('Erreur sqlState : ' + erreur);
+                console.log(`Erreur sqlState ${erreur.sqlState} : ${erreur.sqlMessage}`);
                 reject(erreur);
             }
             else{
-                resolve(resultat);
+                resolve(resultat.rows);
             }
         })
     })
@@ -84,14 +91,16 @@ const modifierLePokemon = (req) =>{
 
 const supprimerLePokemon = (id) => {
     return new Promise((resolve, reject) => {
-        const requete = 'DELETE FROM pokemon WHERE id = ?';
+        const requete = 'DELETE FROM pokemon WHERE id = $1';
         const params = [id];
 
         db.query(requete, params, (erreur, resultat) => {
             if (erreur) {
+                console.log('Erreur sqlState : ' + erreur);
+                console.log(`Erreur sqlState ${erreur.sqlState} : ${erreur.sqlMessage}`);
                 reject(erreur);
             } else {
-                resolve(resultat);
+                resolve(resultat.rows);
             }
         });
     });
